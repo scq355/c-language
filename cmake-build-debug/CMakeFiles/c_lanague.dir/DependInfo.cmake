73d2d18d2a3200c50ba3b9993ca4db585c75db46
孙常青 <nwpuscq355@163.com>
2019-11-16 16:40:46 +0800

    .idea文件忽略

---
+++ cmake-build-debug/CMakeFiles/c_lanague.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sunchangqing/workspaces/c-lanague/chapter01/chapter01.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter01/chapter01.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter02/chapter02.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter02/chapter02.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter03/chapter03.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter03/chapter03.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter04/chapter04.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter04/chapter04.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter05/chapter05.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter05/chapter05.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter06/chapter06.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter06/chapter06.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter07/chapter07.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter07/chapter07.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter08/chapter08.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter08/chapter08.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter10/chapter10.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter10/chapter10.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter11/chapter11.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter11/chapter11.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/chapter13/chapter13.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/chapter13/chapter13.cpp.o"
+  "/Users/sunchangqing/workspaces/c-lanague/main.cpp" "/Users/sunchangqing/workspaces/c-lanague/cmake-build-debug/CMakeFiles/c_lanague.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/c_lanague.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_lanague.dir/chapter01/chapter01.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter02/chapter02.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter03/chapter03.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter04/chapter04.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter05/chapter05.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter06/chapter06.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter07/chapter07.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter08/chapter08.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter10/chapter10.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter11/chapter11.cpp.o"
+  "CMakeFiles/c_lanague.dir/chapter13/chapter13.cpp.o"
+  "CMakeFiles/c_lanague.dir/main.cpp.o"
+  "c_lanague"
+  "c_lanague.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/c_lanague.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
